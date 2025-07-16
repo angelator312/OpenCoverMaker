db68208c9246595a316373a735b41e01904b1685
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-07-16 19:01:06 +0300

    Added editor page

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,9 @@
+import AppShellTemplate from "@/components/AppShellTemplate";
+
+export default function EditorPage() {
+  return (
+    <AppShellTemplate is_in_editor={true}>
+      <div>Editor page</div>
+    </AppShellTemplate>
+  );
+}
