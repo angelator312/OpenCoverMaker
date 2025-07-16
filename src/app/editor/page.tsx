357d26b42897bb478594a7ab57aa0e075287e275
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-07-16 19:40:25 +0300

    Added toggle lyrics button

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import AppShellTemplate from "@/components/AppShellTemplate";
+import { Button, Collapse, Textarea } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 export default function EditorPage() {
+  const [opened_old_lyrics, { toggle: toggle_old_lyrics }] =
+    useDisclosure(false);
   return (
     <AppShellTemplate is_in_editor={true}>
-      <div>Editor page</div>
+      <Button onClick={toggle_old_lyrics}>Toggle Old Lyrics</Button>
+      <Collapse in={opened_old_lyrics}>
+        <Textarea autosize resize="both"></Textarea>
+      </Collapse>
     </AppShellTemplate>
   );
 }
