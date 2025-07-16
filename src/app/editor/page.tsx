9bc9609440862f861eaabcc3d816dc640d3ba741
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-07-16 21:19:22 +0300

    Added settings

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,17 +1,44 @@
 "use client";
 import AppShellTemplate from "@/components/AppShellTemplate";
-import { Button, Collapse, Textarea } from "@mantine/core";
+import {
+  Button,
+  Collapse,
+  Group,
+  Space,
+  Textarea,
+  TextInput,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 
 export default function EditorPage() {
-  const [opened_old_lyrics, { toggle: toggle_old_lyrics }] =
-    useDisclosure(false);
+  const [opened_settings, { toggle: toggle_old_lyrics }] = useDisclosure(true);
   return (
     <AppShellTemplate is_in_editor={true}>
-      <Button onClick={toggle_old_lyrics}>Toggle Old Lyrics</Button>
-      <Collapse in={opened_old_lyrics}>
-        <Textarea autosize resize="both"></Textarea>
+      <Collapse in={opened_settings}>
+        <Group>
+          <TextInput
+            label="Original song name"
+            description="Input the name of the original song."
+          />
+          <TextInput
+            label="The name of the cover/parody"
+            description="Input the name of the new song."
+          />
+          {/* <TextInput
+            label="URL for lyrics"
+            description="Paste URL for lyrics."
+            placeholder="genius.com or azlyrics.com/ "
+          /> */}
+          <Textarea
+            label={"Original Lyrics"}
+            description="Paste the original lyrics of the song" // Think about better description
+            autosize
+            resize="both"
+          ></Textarea>
+        </Group>
       </Collapse>
+      <Space h="md" />
+      <Button onClick={toggle_old_lyrics}>Toggle settings visible</Button>
     </AppShellTemplate>
   );
 }
