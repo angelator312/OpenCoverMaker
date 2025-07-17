0f010ccdd36fb45f47512cccfb837a84960473eb
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-07-17 14:36:27 +0300

    added type to square brackets rendere

--- src/components/SquareBracketLineEdit.tsx
+++ src/components/SquareBracketLineEdit.tsx
@@ -1,32 +1,66 @@
-import { Group, TextInput } from "@mantine/core";
-const regex_for_use = /\[(.*?):(.*)\]/g;
+import { Group, Text, TextInput } from "@mantine/core";
+import { useEffect, useState } from "react";
+const regex_for_verse = /\[(.*?):(.*)\]/g;
+const regex_for_chorus = /\[Chorus\]/g;
+const regexes = [regex_for_verse, regex_for_chorus];
+enum TypeEnum {
+  Verse = 0,
+  Chorus = 1,
+  Bridge = 2,
+  Prechorus = 3,
+  Intro = 4,
+  Outro = 5,
+  Tag = 6,
+  Comment = 7,
+}
 export default function SquareBracketLineEdit({
   new_lyrics,
   setNewLyrics,
   setAutoFocus,
   autoFocus = false,
+  type = TypeEnum.Verse,
 }: {
+  type?: TypeEnum;
   new_lyrics: string;
   autoFocus?: boolean;
   setNewLyrics: (e: string) => void;
   setAutoFocus: (e: number) => void;
 }) {
-  let match = regex_for_use.exec(new_lyrics);
-  if (!match) return "Error";
-  const what = match.slice(1)[0];
-  const value = match.slice(1)[1];
-  console.log(what, ":", value);
+  const [what, setWhat] = useState("");
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    let match = regexes[type].exec(new_lyrics);
+    if (!match) return;
+    setWhat(match.slice(1)[0]);
+    setValue(match.slice(1)[1]);
+    console.log(what, ":", value);
+  }, [new_lyrics]);
+  useEffect(() => {
+    setNewLyrics(`[${what}:${value}]`);
+  }, [what, value, type]);
   return (
-    <Group grow>
+    <Group>
+      <Text>[</Text>
       <TextInput
         autoFocus={autoFocus}
         onKeyUp={(e) => {
           if (e.key.startsWith("Enter")) setAutoFocus(-1);
         }}
         value={what}
-        onChange={(e) => setNewLyrics(e.currentTarget.value)}
+        onChange={(e) => setWhat(e.currentTarget.value)}
         // inputSize="100"
       />
+      <Text>:</Text>
+      <TextInput
+        autoFocus={autoFocus}
+        onKeyUp={(e) => {
+          if (e.key.startsWith("Enter")) setAutoFocus(-1);
+        }}
+        value={value}
+        onChange={(e) => setValue(e.currentTarget.value)}
+        // inputSize="100"
+      />
+      <Text>]</Text>
     </Group>
   );
 }
