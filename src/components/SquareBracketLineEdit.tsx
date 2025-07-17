633739c1b23b5d4e890c77de28ca7e7c21492948
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-07-17 14:48:34 +0300

    added type dropdown in Square Bracket Render

--- src/components/SquareBracketLineEdit.tsx
+++ src/components/SquareBracketLineEdit.tsx
@@ -1,8 +1,33 @@
-import { Group, Text, TextInput } from "@mantine/core";
+import { Group, Select, Text, TextInput } from "@mantine/core";
 import { useEffect, useState } from "react";
-const regex_for_verse = /\[(.*?):(.*)\]/g;
+const regex_for_verse = /\[Verse \d:(.*)\]/g;
 const regex_for_chorus = /\[Chorus\]/g;
-const regexes = [regex_for_verse, regex_for_chorus];
+const regex_for_bridge = /\[Bridge\]/g;
+const regex_for_prechorus = /\[Pre-chorus\]/g;
+const regex_for_intro = /\[Intro\]/g;
+const regex_for_outro = /\[Outro\]/g;
+const regex_for_tag = /\[Tag-\]/g;
+const regex_for_comment = /\[Comment-\]/g;
+const regexes = [
+  regex_for_verse,
+  regex_for_chorus,
+  regex_for_bridge,
+  regex_for_prechorus,
+  regex_for_intro,
+  regex_for_outro,
+  regex_for_tag,
+  regex_for_comment,
+];
+const names = [
+  "Verse",
+  "Chorus",
+  "Bridge",
+  "Pre-chorus",
+  "Intro",
+  "Outro",
+  "Tag",
+  "Comment",
+];
 enum TypeEnum {
   Verse = 0,
   Chorus = 1,
@@ -18,36 +43,31 @@ export default function SquareBracketLineEdit({
   setNewLyrics,
   setAutoFocus,
   autoFocus = false,
-  type = TypeEnum.Verse,
 }: {
-  type?: TypeEnum;
   new_lyrics: string;
   autoFocus?: boolean;
   setNewLyrics: (e: string) => void;
   setAutoFocus: (e: number) => void;
 }) {
-  const [what, setWhat] = useState("");
+  const [type, setType] = useState(typeFromString(new_lyrics));
   const [value, setValue] = useState("");
   useEffect(() => {
+    setType(typeFromString(new_lyrics));
     let match = regexes[type].exec(new_lyrics);
     if (!match) return;
-    setWhat(match.slice(1)[0]);
-    setValue(match.slice(1)[1]);
-    console.log(what, ":", value);
-  }, [new_lyrics]);
+    setValue(match.slice(1)[0]);
+    console.log(type, ":", value);
+  }, [new_lyrics, type]);
   useEffect(() => {
-    setNewLyrics(`[${what}:${value}]`);
-  }, [what, value, type]);
+    setNewLyrics(`[${names[type]}:${value}]`);
+  }, [type, value]);
   return (
     <Group>
       <Text>[</Text>
-      <TextInput
-        autoFocus={autoFocus}
-        onKeyUp={(e) => {
-          if (e.key.startsWith("Enter")) setAutoFocus(-1);
-        }}
-        value={what}
-        onChange={(e) => setWhat(e.currentTarget.value)}
+      <Select
+        value={names[type]}
+        data={names}
+        onChange={(e) => setType(typeFromName(e ?? ""))}
         // inputSize="100"
       />
       <Text>:</Text>
@@ -63,4 +83,19 @@ export default function SquareBracketLineEdit({
       <Text>]</Text>
     </Group>
   );
+}
+
+export function typeFromString(str: string) {
+  if (str.match(regex_for_verse)) return TypeEnum.Verse;
+  if (str.match(regex_for_chorus)) return TypeEnum.Chorus;
+  if (str.match(regex_for_bridge)) return TypeEnum.Bridge;
+  if (str.match(regex_for_prechorus)) return TypeEnum.Prechorus;
+  if (str.match(regex_for_intro)) return TypeEnum.Intro;
+  if (str.match(regex_for_outro)) return TypeEnum.Outro;
+  if (str.match(regex_for_tag)) return TypeEnum.Tag;
+  if (str.match(regex_for_comment)) return TypeEnum.Comment;
+  return TypeEnum.Verse;
+}
+function typeFromName(name: string) {
+  return names.indexOf(name);
 }
